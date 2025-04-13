8ee6d94ba0444fc7048737d425b4fcffa312d3e8
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-04-13 19:54:21 +0530

    Deploy app on Vercel

--- app/payment/page.js
+++ app/payment/page.js
@@ -1,42 +1,10 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { loadStripe } from "@stripe/stripe-js"; // Import Stripe loadStripe
-import { Elements } from "@stripe/react-stripe-js"; // Import Elements
-import CheckoutForm from "./../../components/Home/CheckoutForm";
+import React, { Suspense } from "react";
+import PaymentPage from "./PaymentPage"; // Separate file
 
-const Payment = () => {
-  const searchParams = useSearchParams();
-  const amountParam = searchParams.get("amount");
-  const [stripePromise, setStripePromise] = useState(null); // Use state to store Stripe instance
-
-  // Ensuring the amount is a valid number
-  const amount = amountParam ? parseFloat(amountParam) : null;
-
-  useEffect(() => {
-    // Only load Stripe if the amount is valid
-    if (amount) {
-      const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
-      setStripePromise(stripe);
-    }
-  }, [amount]);
-
-  // If amount is invalid or Stripe is not loaded yet, you can return a loading message or fallback UI
-  if (!amount || !stripePromise) {
-    return <div>Loading...</div>;
-  }
-
-  const options = {
-    mode: "payment",
-    amount: Math.round(amount * 100), // Multiply by 100 for Stripe's smallest unit (cents)
-    currency: "inr",
-  };
-
+export default function Page() {
   return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm amount={amount} />
-    </Elements>
+    <Suspense fallback={<div>Loading payment page...</div>}>
+      <PaymentPage />
+    </Suspense>
   );
-};
-
-export default Payment;
+}
