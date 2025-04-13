e2a5b815db874227871d46d06abbe8030a53c5d1
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-04-13 18:59:51 +0530

    switched to type script and instlled stripe

--- components/Home/CarListOptions.js
+++ components/Home/CarListOptions.js
@@ -1,9 +1,12 @@
 import CarsDataList from "@/utils/CarsDataList";
 import React, { useState } from "react";
 import CarListItem from "./CarListItem";
+import { useRouter } from "next/navigation";
 
 const CarListOptions = ({ distance }) => {
   const [activeIndex, setActiveIndex] = useState();
+  const [selectedCar, setSelectedCar] = useState();
+  const router = useRouter();
 
   return (
     <div className="mt-5 overflow-auto h-[300px]">
@@ -16,11 +19,25 @@ const CarListOptions = ({ distance }) => {
               ? "border-[2px] border-black rounded-2xl"
               : null
           }`}
-          onClick={() => setActiveIndex(index)}
+          onClick={() => {
+            setActiveIndex(index);
+            setSelectedCar(item);
+          }}
         >
           <CarListItem car={item} distance={distance} />
         </div>
       ))}
+      {selectedCar && <CarListItem car={selectedCar} distance={distance} /> ? (
+        <div className="flex items-center justify-between bottom-1 fixed rounded-lg  bg-white p-3 shadow-xl w-full md:w-[30%] border-[1px] ">
+          <h2>Make Payment For</h2>
+          <button
+            className="bg-black text-white py-2 px-4 rounded-lg"
+            onClick={() => router.push("/payment")}
+          >
+            Request {selectedCar.name}
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 };
